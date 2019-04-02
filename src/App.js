aefd0d19e52803db533a199bcd67e7fba30140c7
hninwaikhin <wai.khinhninwai@gmail.com>
2019-04-02 17:31:14 +0630

    ReactApp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter,Route } from 'react-router-dom';
+import { PATH_POPULAR, PATH_TOP_RATED, PATH_UPCOMING, PATH_WATCHLIST } from './api';
+import Header from './components/Header';
+import Main from './components/Main';
+import Discover from './components/Discover';
+import WatchList from './components/WatchList';
+import SearchResults from './components/SearchResults';
+import { NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+      loading: true,
+      favorites: {},
+      watchLater: {},
+      ...this.defaulFilterstState
+    };
+  }
+
+  defaulFilterstState = {
+    filters: {     
+      sort_by: {
+        value: 'vote_average',
+        label: 'Rating'
+      },
+      order: {
+        value: 'desc',
+        label: 'Descending'
+      },
+      year: new Date().getFullYear()
+    }
+  } 
+
+  updateStateWithFilters = (filters) => this.setState({ filters })
+
+  resetFilters = () => this.setState(this.defaultState)
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">        
+          <div className="App-sidebar-wrapper"> 
+            <div className="left-sidebar-menu">
+              <NavLink exact={true} to="/" activeClassName="is-active">
+                <i class="fa fal fa-bars"></i>
+              </NavLink>
+            </div>
+            <div className="left-sidebar-menu">
+              <NavLink exact={true} to="/watchList" activeClassName="is-active">
+                <i class="fa fa-star-o"></i>
+              </NavLink>   
+            </div>         
+          </div>
+          <div className="App-content-wrapper">
+            <Header
+              user={this.state.user} filters={this.state.filters}
+              updateFilters={this.updateStateWithFilters}
+              resetFilters={this.resetFilters}
+            />
+            <Route exact path="/"
+              render={()=><Discover
+                title="Discover"
+                updateFilters={this.updateStateWithFilters}
+                filters={this.state.filters} />}
+             />
+            <Route exact path="/popular"
+              render={()=><Main
+                title="Popular"
+                section={PATH_POPULAR} />}
+            />
+            <Route exact path="/top-rated"
+              render={()=><Main
+                title="Top Rated"
+                section={PATH_TOP_RATED}  />}
+            />
+            <Route exact path="/coming-soon"
+              render={()=><Main
+                title="Coming Soon"
+                section={PATH_UPCOMING}  />}
+            />
+            <Route exact path="/watchList"
+              render={()=><WatchList
+                title="Watch List"
+                section={PATH_WATCHLIST}  />}
+            />
+            <Route path="/search" component={SearchResults}/>            
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+export const API_KEY = 'b5fb19524ed85b27636573608f6285c4';
+export const PATH_BASE = 'https://api.themoviedb.org/3';
+
+export const PATH_SEARCH = '/search';
+
+export const PATH_MOVIE = '/movie';
+export const PATH_DISCOVER = '/discover';
+export const PATH_POPULAR = '/popular';
+export const PATH_TOP_RATED = '/top_rated';
+export const PATH_UPCOMING = '/upcoming';
+export const PATH_WATCHLIST = '/watchList';
+
+export const DEFAULT_PAGE = 1;
+export const PATH_PAGE = 'page='
