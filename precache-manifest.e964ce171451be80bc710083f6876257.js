70c7a8f56fb389d8839006a13a5e14c4feb984d3
hninwaikhin <wai.khinhninwai@gmail.com>
2019-04-02 16:54:48 +0630

    Updates

---
+++ precache-manifest.e964ce171451be80bc710083f6876257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "64758bd2ffc95f500be3",
+    "url": "/react-test/static/js/main.322d65f5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-test/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "64758bd2ffc95f500be3",
+    "url": "/react-test/static/css/main.b1099e85.chunk.css"
+  },
+  {
+    "revision": "8998d62d98f9fb198d6506b5875911e9",
+    "url": "/react-test/index.html"
+  }
+];
